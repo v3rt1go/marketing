979dacd571b4ec878c53b476858ea6eb4da19d95
Alex Griciuc <agriciuc@gmail.com>
2015-09-22 02:09:52 +0300

    changed index.js to write reponse in browser

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
-var static = require('node-static');
+// var static = require('node-static');
 
 //
 // Create a node-static server instance to serve the './public' folder
 //
-var file = new static.Server('./public');
+// var file = new static.Server('./public');
 
 require('http').createServer(function(request, response) {
-  request.addListener('end', function() {
-    //
-    // Serve files!
-    //
-    file.serve(request, response);
-  }).resume();
+  // request.addListener('end', function() {
+  //   //
+  //   // Serve files!
+  //   //
+  //   file.serve(request, response);
+  // }).resume();
+  response.write("Foo");
 }).listen(8081);
