df1b9355313eb94032f978e519b5d6e767be231f
Alex Griciuc <agriciuc@outlook.com>
2015-09-21 16:35:55 +0300

    more work on deploy scripts

--- scripts/post_receive.sh
+++ scripts/post_receive.sh
@@ -1,13 +1,28 @@
 #!/bin/bash
-echo "Hi Alex"
+
+# Setting up script variables
+WEB_ROOT = "~/www"
+APP_NAME = "boilerplate"
+
+APP_ROOT = $WEB_ROOT/apps
+TEMP_ROOT = $WEB_ROOT/temp
+REPO_ROOT = $WEB_ROOT/repos
+SHARED_ROOT = $WEB_ROOT/shared
+
+APP_DIR = $APP_ROOT/$APP_NAME
+TEMP_DIR = $TEMP_ROOT/$APP_NAME
+REPO_DIR = $REPO_ROOT/$APP_NAME
+SHARED_DIR = $SHARED_ROOT/$APP_NAME
+
+echo "Running deploy script ..."
 
 # Clone the repo in the temp dir
-cd /home/v3rt1go/www/temp
-echo "Cloning git repo in temp directory"
-git clone /home/v3rt1go/www/repos/boilerplate boilerplate
+cd $TEMP_ROOT
+echo "Cloning $REPO_DIR in $TEMP_DIR"
+git clone $REPO_DIR $APP_NAME
 
 # Install project dependencies on the cloned dir
-cd /home/v3rt1go/www/temp/boilerplate
+cd $TEMP_DIR
 echo "Setting node version"
 # If we have nvm we should set the required node version here
 echo "Installing npm dependencies"
@@ -15,16 +30,27 @@ npm install
 echo "Installing bower dependencies"
 bower install
 
+# Create symlinks to data and images folders
+# Before we create the symlinks we must make sure that data is copied over into the
+# /shared/boilerplate/data & /shared/boilerplate/images folders, so we don't loose it by accident
+# Also, make sure /upload/data and /upload/images are added in .gitignore - we do not want
+# user data to end up in our git repo
+echo "Linking user images and data"
+ln -s $SHARED_DIR/images $TEMP_DIR/upload/images
+ln -s $SHARED_DIR/data $TEMP_DIR/upload/data
+
 # Turn off pm2 for this project
-echo "Turning off the application"
-pm2 stop boilerplate
+echo "Turning off the pm2 application"
+pm2 stop $APP_NAME
 
 # Move the temp bits & built stuff to our project in /apps and delete old files
 echo "Deleting the old application"
-rm -R /home/v3rt1go/www/apps/boilerplate
+rm -R $APP_DIR
 echo "Moving the new application"
-mv /home/v3rt1go/www/temp/boilerplate /home/v3rt1go/www
+mv $TEMP_DIR $APP_ROOT
 
 # Turn pm2 back on for this site
-echo "Starting the application"
-pm2 start /home/v3rt1go/www/app/boilerplate/index.js --name "boilerplate"
+echo "Starting the pm2 application"
+pm2 start $APP_DIR/index.js --name "$APP_NAME"
+
+echo "All done! Please check your app."
---
+++ scripts/web_setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Copy nginx config file from app server
+
+# Create simlink to /sites-enabled
+
+# Restart nginx
