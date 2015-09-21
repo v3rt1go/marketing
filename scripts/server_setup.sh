05cd660aeb4b0ae07fac375e442f58031b7111a3
Alex Griciuc <agriciuc@gmail.com>
2015-09-21 10:42:28 +0300

    added shared directories in setup script for images & data

--- scripts/server_setup.sh
+++ scripts/server_setup.sh
@@ -13,6 +13,10 @@ mkdir ~/www/repos
 # Create a temp dir for holding live pushes
 mkdir ~/www/temp
 
+# Create shared directories for images and data
+mkdir -p ~/www/shared/boilerplate/images
+mkdir -p ~/www/shared/boilerplate/data
+
 # Create a bare git repo for this apps
 # A bare repo means it will use that folder for the git repo instead of the usual
 # .git folder. Usually when we say git init it will create the .git folder and put
@@ -24,4 +28,6 @@ mkdir ~/www/repos/boilerplate && git init ~/www/repos/boilerplate --bare
 # Create with vim post-recieve the script and add #!/bin/bash at the first line and echo "Hi Alex" on the second line
 # Give x permissions to the file with chmod +x post-recieve
 # From the project folder (~/Projects/boilerplate) add a git remote to /www/repos/boilerplate
-# git remote add deploy /Users/v3rt1go/www/repos/boilerplate
+# git remote add deploy /home/v3rt1go/www/repos/boilerplate
+# add and commit the changes
+# git push deploy master ---- we should see remote: Hi Alex in the output
